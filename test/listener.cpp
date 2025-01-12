e048c83eff35bac37df40201aa7b7f3a25639915
Emad Aldeen Hammoude <ehammoud@lab1r2s4.42abudhabi.ae>
2025-01-12 18:54:25 +0400

    Socket handling

--- test/listener.cpp
+++ test/listener.cpp
@@ -4,9 +4,11 @@
 #include <stdio.h>  
 #include <stdlib.h>  
 #include <string.h>  
-#include <sys/socket.h>  
-#include <unistd.h>  
-#define PORT 8080  
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define PORT 8080
+
 int main(int argc, char const* argv[])  
 {  
        int server_fd, new_socket, valread;  
@@ -14,7 +16,7 @@ int main(int argc, char const* argv[])
        int opt = 1;  
        int addrlen = sizeof(address);  
        char buffer[1024] = { 0 };  
-       char* hello = "Hello from server";  
+       char hello[] = "Hello from server";  
   
        // Creating socket file descriptor  
        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {  
@@ -24,11 +26,11 @@ int main(int argc, char const* argv[])
   
        // Forcefully attaching socket to port 8080  
        if (setsockopt(server_fd, SOL_SOCKET,  
-                               SO_REUSEADDR | SO_REUSEPORT, &opt,  
+                               SO_REUSEADDR, &opt,  
                                sizeof(opt))) {  
                perror("setsockopt");  
                exit(EXIT_FAILURE);  
-       }  
+       }
        address.sin_family = AF_INET;  
        address.sin_addr.s_addr = INADDR_ANY;  
        address.sin_port = htons(PORT);  
@@ -51,7 +53,7 @@ int main(int argc, char const* argv[])
                perror("accept");  
                exit(EXIT_FAILURE);  
        }  
-       valread = read(new_socket, buffer, 1024);  
+       valread = read(new_socket, buffer, 1024);
        printf("%s\n", buffer);  
        send(new_socket, hello, strlen(hello), 0);  
        printf("Hello message sent\n");  
@@ -59,6 +61,6 @@ int main(int argc, char const* argv[])
        // closing the connected socket  
        close(new_socket);  
        // closing the listening socket  
-       shutdown(server_fd, SHUT_RDWR);  
+       close(server_fd);  
        return 0;  
 }  
