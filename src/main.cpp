c178769d541f03e282a606c6253a6bedd2b982f0
MonkeyCofi <piercediet06@gmail.com>
2025-01-28 19:19:32 +0400

    made some changes with the server sock_fds

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 int main(int ac, char **av)
 {
+	unlink("0.0.0.0");
 	if (ac != 2)
 	{
 		std::cerr << "Error\nInvalid number of arguments!\nUsage: ./webserv <configuration_file.conf>\n";
@@ -44,14 +45,34 @@ int main(int ac, char **av)
 	{
 		struct pollfd	temp;
 		temp.fd = listeners.at(i)->returnSocket(i);
-		temp.events = 0;
+		temp.events = POLLIN;
 		temp.revents = 0;
 		sock_fds.push_back(temp);
 	}
 
-	// while (1)
-	// {
-	// 	int res = poll(&sock_fds[0], sock_fds.size(), 100);
-	// 	if ()
-	// }
+	 while (1)
+	 {
+	 	int res = poll(&sock_fds[0], sock_fds.size(), 100);
+		if (res == -1)
+		{
+			perror("poll");
+			exit(EXIT_FAILURE);
+		}
+		for (unsigned int i = 0; i < listeners.size(); i++)
+		{
+			std::cout << sock_fds.at(i).revents << "\n";
+			if (sock_fds.at(i).revents & POLLIN)
+			{
+				struct sockaddr_in	client;
+				socklen_t	len = sizeof(client);
+				int acc_sock = accept(sock_fds.at(i).fd, (sockaddr *)&client, &len);
+				char buf[1024];
+				recv(acc_sock, buf, sizeof(buf), 0);
+				std::cout << buf << "\n";
+				close(acc_sock);
+			}
+			else if (sock_fds.at(i).revents & POLLOUT)
+				std::cout << "Fd " << sock_fds.at(i).fd << " is ready for output\n";
+		}
+	 }
 }
